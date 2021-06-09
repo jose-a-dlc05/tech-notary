222761094f8c601034acd4a95b80f920973b4f82
Jose De La Cruz <delacruz@joses-air.myfiosgateway.com>
2021-06-09 19:53:13 -0400

    Added toggle functionality to hamburger icon

--- src/components/navbar/Navbar.component.jsx
+++ src/components/navbar/Navbar.component.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.styles.scss";
 
 export const Navbar = () => {
-	const handleNavClick = () => {};
+	const [menuStyles, setMenuStyles] = useState(["nav-items"]);
+
+	const handleNavClick = () => {
+		const newStyles = [
+			"nav-items",
+			!menuStyles.includes("toggleDisplay") && "toggleDisplay",
+		].filter((style) => style);
+		setMenuStyles(newStyles);
+	};
 
 	return (
 		<nav>
 			<div className='container'>
 				<div className='topbar'>
 					<h1 className='logo'>Blog</h1>
-					<div className='burger'>
+					<div className='burger' onClick={handleNavClick}>
 						<div className='line1'></div>
 						<div className='line2'></div>
 						<div className='line3'></div>
 					</div>
 				</div>
-				<ul className='nav-items'>
+				<ul className={menuStyles}>
 					<li>Home</li>
 					<li>Profile</li>
 					<li>Write Post</li>
