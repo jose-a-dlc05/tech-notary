f2d11b9d7cf715f3ba446c5857e0d14cbc313a6c
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-07-12 18:30:54 -0400

    Changed title on build

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,17 @@ import App from "./App";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
-describe("Testing App Component", () => {
+describe.only("Testing App Component", () => {
 	test("App Component rendered to page", () => {
+		// Arrange
 		const comp = render(
 			<MemoryRouter>
 				<App />
 			</MemoryRouter>
 		);
+		// Act
 		comp.debug();
+		console.log(comp.container.getElementsByClassName("card-list")[0]);
+		//
 	});
 });
